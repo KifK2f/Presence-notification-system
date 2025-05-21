04fdd963f548d6cba51ff8367220df3a711fa4ae
KifK2f <fianyofriedo2@gmail.com>
2025-05-21 09:48:58 +0000

    Initial project

---
+++ src/test/java/com/friedo/ossan_asur/notification_presence/NotificationPresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friedo.ossan_asur.notification_presence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPresenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
