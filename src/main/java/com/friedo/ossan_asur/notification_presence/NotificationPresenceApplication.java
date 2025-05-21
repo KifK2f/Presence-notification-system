762bb5bbb19edb9552ca6f4a167490e0b62863ce
KifK2f <fianyofriedo2@gmail.com>
2025-05-21 09:46:59 +0000

    Initial project

---
+++ src/main/java/com/friedo/ossan_asur/notification_presence/NotificationPresenceApplication.java
@@ -0,0 +1,13 @@
+package com.friedo.ossan_asur.notification_presence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationPresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationPresenceApplication.class, args);
+	}
+
+}
